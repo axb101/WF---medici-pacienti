d92d3c5e264b0fb0c71510b91bcbceaf9d9cc11c
Alex Barbulescu <alex.barbulescu@businessmart.ro>
2022-03-23 20:49:12 +0200

    test1

--- WF - medici-pacienti/Form1.Designer.cs
+++ WF - medici-pacienti/Form1.Designer.cs
@@ -38,6 +38,7 @@ namespace WF___medici_pacienti
             this.lvpCnp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.lvpCardS = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -110,11 +111,21 @@ namespace WF___medici_pacienti
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(308, 324);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Editeaza Medic";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1188, 683);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView2);
             this.Controls.Add(this.listView1);
@@ -135,6 +146,7 @@ namespace WF___medici_pacienti
         private System.Windows.Forms.ColumnHeader lvpCnp;
         private System.Windows.Forms.ColumnHeader lvpCardS;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
